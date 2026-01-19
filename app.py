7166ad049c7c2457dc5218b9be62186efae5bdf4
Ethan Chua <ethancqy@gmail.com>
2026-01-19 15:06:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,6 @@ def get_face_recognition_logs():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
